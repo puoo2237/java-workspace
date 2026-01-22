3385f27181fe3f0bfdf200a2eb20a087d5406d3e
seunghee <seungjj1104@gmail.com>
2026-01-22 15:44:18 +0900

    quiz(3일차;메소드 추가)

--- ex03/src/ex02/MainClass05.java
+++ ex03/src/ex02/MainClass05.java
@@ -1,28 +1,35 @@
 package ex02;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainClass05 {
 	 public static void main(String[] args) {
 		TestClass05 t01 = new TestClass05();
 		
 		System.out.println("======= 1번 =======");
-		t01.getName("홍길동");
+		String name = "홍길동";
+		t01.printName(name);
 		
 		System.out.println("======= 2번 =======");
-		t01.getNames("홍가", "김가", "최가");
+		String[] names = {"홍길동", "김강우", "최가네"};
+		t01.printNames(names);
 		
 		System.out.println("======= 3번 =======");
 		int n1 = 24;
 		int n2 = 25;
-		int res3 = t01.intSum(n1, n2);
-		System.out.println(res3);
+		t01.printSum(t01.intSum(n1, n2));
 		
 		System.out.println("======= 4번 =======");
 		ArrayList<Integer> num = new ArrayList<Integer>();
 		num.add(n1);
 		num.add(n2);
-		int res4 = t01.arrSum(num);  
-		System.out.println(res4);
+		t01.printSum(t01.arrSum(num));
+		
+		System.out.println("======= 5번 =======");
+		HashMap<String, Integer> n = new HashMap<String, Integer>();
+		n.put("num1", n1);
+		n.put("num2", n2);
+		t01.printSum(t01.mapSum(n));
 	}
 }
--- ex03/src/ex02/TestClass05.java
+++ ex03/src/ex02/TestClass05.java
@@ -1,23 +1,27 @@
 package ex02;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 
 public class TestClass05 {
 	// 1. 이름을 입력받아 출력하는 프로그램
-	public void getName(String n) {
-		System.out.println(n);
+	public void printName(String name) {
+		System.out.println("이름: " + name);
 	}
 
 	// 2. 3개의 이름을 입력받아 출력(배열 활용; 입출력; main 기능)
-	public void getNames(String s1, String s2, String s3) {
-		String[] name = {s1, s2, s3};
-		for (String s : name) {
-			this.getName(s);
+	public void printNames(String[] names) {
+		for (String name : names) {
+			this.printName(name);
 		}
 	}
 
 	// 3. 두 수의 합을 입, 출, 연산, main 기능으로 만들기
+	public void printSum(int num) {
+		System.out.println("두 수의 합은 " + num);
+	}
+
 	public int intSum(int n1, int n2) {
 		return n1 + n2;
 	}
@@ -35,8 +39,12 @@ public class TestClass05 {
 
 	// 3. 두 수의 합을 입, 출, 연산, 메인 기능으로 만들기
 	// 5. 3번의 내용을 Map을 각 기능별로 만들기
-	public int mapSum() {
-
-		return 0;
+	public int mapSum(HashMap<String, Integer> num) {
+		int sum = 0;
+		for (int v:num.values()) {
+			sum += v;
+		}
+		return sum;
 	}
+
 }
